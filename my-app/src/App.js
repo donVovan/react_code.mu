2422d8ca227dbf26ba3fed8d1613ae9d74223950
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-03-21 09:01:47 +0300

    добавил Checklist.jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,11 @@
 //⊗jsrtPmCpRCC
 import React, { useState } from "react";
 import "./App.css";
+import Checklist from "./Checklist";
 
 function App() {
-    const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-    const [editIndex, setEditIndex] = useState(null);
-
-    function startEdit(index) {
-        setEditIndex(index);
-    }
-
-    function changeItem(event) {
-        const newNotes = [...notes];
-        newNotes[editIndex] = event.target.value;
-        setNotes(newNotes);
-    }
-
-    const result = notes.map((note, index) => (
-        <p key={index} onClick={() => startEdit(index)}>
-            {note}
-        </p>
-    ));
-
     return (
-        <div>
-            {result}
-            {editIndex !== null && (
-                <input
-                    value={notes[editIndex]}
-                    onChange={changeItem}
-                    onBlur={() => setEditIndex(null)}
-                />
-            )}
-        </div>
+        <Checklist />
     );
 }
 
---
+++ my-app/src/Checklist.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+function Checklist() {
+    const [items, setItems] = useState([]);
+    const [text, setText] = useState('');
+
+    const handleInputChange = event => {
+        setText(event.target.value);
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if (!text || /^\s*$/.test(text)) {
+            return;
+        }
+
+        setItems([...items, { text, checked: false }]);
+        setText('');
+    };
+
+    const handleDelete = i => {
+        setItems(items.filter((_, index) => index !== i));
+    };
+
+    const handleEdit = (i, newText) => {
+        const newItems = [...items];
+        newItems[i].text = newText;
+        setItems(newItems);
+        setText('');
+    };
+
+    const handleCheck = i => {
+        const newItems = [...items];
+        newItems[i].checked = !newItems[i].checked;
+        setItems(newItems);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={text} onChange={handleInputChange} />
+                <button type="submit">Добавить</button>
+            </form>
+            <ul>
+                {items.map((item, i) => (
+                    <li key={i}>
+            <span style={item.checked ? { textDecoration: 'line-through' } : null}>
+              {item.text}
+            </span>
+                        <button onClick={() => handleCheck(i)}>Отметить</button>
+                        <button onClick={() => handleDelete(i)}>Удалить</button>
+                        {item.checked ? null : (
+                            <>
+                                <button onClick={() => setText(item.text)}>Редактировать</button>
+                                <button onClick={() => handleEdit(i, text)}>Сохранить</button>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Checklist;
